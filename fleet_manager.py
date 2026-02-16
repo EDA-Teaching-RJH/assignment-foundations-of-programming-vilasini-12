97234dbb5c72bcbba754e0372e79f7ac0e08b562
vilasini-12 <vl11@kent.ac.uk>
2026-02-16 04:50:13 +0000

    all comments added

--- fleet_manager.py
+++ fleet_manager.py
@@ -25,7 +25,7 @@ def display_menu():
   print(f"Welcome {user_name}, please view the MENU: \n1. View Crew\n2. Add Member\n3. Remove Member\n4. Update Rank"
         "\n5. Display Roster\n6. Search Crew\n7. Members in Certain Division\n8. Crew Payroll" 
         "\n9. Analyse Data: No of Captains and Commanders\n10. Exit")
-  option = input("Select option: ")   #asks user to input option
+  option = input("Select option: ").strip()   #asks user to input option and used .strip() to remove spaces
   return option
 
 
@@ -93,81 +93,99 @@ def display_roster(names, ranks, divs, ids):
   iterates through the list using 'range(len(names))'
   and prints a formatted table of crew.
   """
-  print("{:<13} {:<13} {:<13} {:<13}".format("Name", "Ranks", "Division", "IDs"))
-  for i in range(len(names)):   #iterates through list 
-    #print(str(names[i]) + " - " + str(ranks[i]) + " - " + str(divs[i]) + " - " + str(ids[i]))    #prints name,rank,division and id for each member
-    print("{:<13} {:<13} {:<13} {:<13}".format(names[i], ranks[i], divs[i], ids[i]))
+  print("{:<13} {:<13} {:<13} {:<13}".format("Name", "Ranks", "Division", "IDs"))   #prints title of the columns
+  for i in range(len(names)):   #iterates through names list 
+    print("{:<13} {:<13} {:<13} {:<13}".format(names[i], ranks[i], divs[i], ids[i]))    #prints each member's details in columms 
 
 def search_crew(names, ranks, divs, ids):
-  search = input("Enter term to find members: ")
-  found = False
-  for i in range(len(names)):
-    if search in str(names[i]):
-      print(str(names[i]) + " - " + str(ranks[i]) + " - " + str(divs[i]) + " - " + str(ids[i]))
-      found = True
-  if not found:
+  """
+  This function takes input of all the lists and 
+  asks user to input a search term and prints 
+  any crew members' names with those terms. 
+  """
+  search = input("Enter term to find members: ").lower()    #asking for search term and used .lower() to make it case-insensitive
+  found = False   #flag to track if match is found
+  for i in range(len(names)):   #iterates through names list
+    if search in str(names[i]).lower():   #used .lower() for case-insensitivity
+      print(str(names[i]) + " - " + str(ranks[i]) + " - " + str(divs[i]) + " - " + str(ids[i]))   #prints name,rank,division and id for member's name containing term
+      found = True    #flag to True as at least one match found
+  if not found:   #if no matches after loop then say no members found
       print("No Members contain that.")
 
 def filter_by_division(names, divs):
-  members = []
-  section = input("What division do you want to check: Command, Operations, Science: ").capitalize()
-  for i in range(len(divs)):
-    if divs[i] == section:
-      members.append(names[i])
+  """
+  This function takes input of names and divs list and 
+  asks user to input certain division name and prints
+  members from the chosen division.
+  """
+  members = []    #empty list to add members name
+  section = input("What division do you want to check: Command, Operations, Science: ").capitalize()    #used .capitalize() to prevent value error
+  for i in range(len(divs)):    #iterates through divs list
+    if divs[i] == section:    #checks if item in list matches user input
+      members.append(names[i])    #add's member's name to members list
   return members
 
 def calculate_payroll(ranks):
-  total_pay = 0
-  for rank in ranks:
-    if rank == "Captain":
-      total_pay += 1000
+  """
+  This function takes input of ranks list and 
+  iterates through list, assign credit values to each rank 
+  and calculates total cost of the crew.
+  """
+  total_pay = 0   #initialise variable to store total payroll cost
+  for rank in ranks:    #iterates through ranks list
+    if rank == "Captain": 
+      total_pay += 1000    #assigning credit value to Captain
     elif rank == "Ensign":
-      total_pay += 200
+      total_pay += 200    #assigning credit value to Ensign
     elif rank == "Lieutenant":
-      total_pay += 700
+      total_pay += 700    #assigning credit value to Lieutenant
     elif rank == "Lt. Commander":
-      total_pay += 800
+      total_pay += 800    #assigning credit value to Lt. Commander
     elif rank == "Commander":
-      total_pay += 900
+      total_pay += 900    #assigning credit value to Commander
     elif rank == "Admiral":
-      total_pay += 1200
+      total_pay += 1200   #assigning credit value to Admiral
     else:
       print("Unknown rank")
-  return total_pay
+  return total_pay    #return total cost of crew
 
 def count_officers(ranks):
-  count = 0
-  for rank in ranks:
-    if rank == "Captain" or rank == "Commander":
-      count = count + 1
-  return count
+  """
+  This function takes input of ranks list and 
+  counts how many "Captains" and "Commanders" are
+  in the crew.
+  """
+  count = 0   #initialise variable to store no of high-rank officers
+  for rank in ranks:    #iterates through ranks list
+    if rank == "Captain" or rank == "Commander":    #checks if rank is Captain or Commander
+      count = count + 1   #count increment if rank is Captain or Commander
+  return count    #returns total no of Captains and Commanders
 
 def main():
-  while True:
-    opt = display_menu()
-
-    if opt == "1":
+  while True:   #infinite loop to show menu until exit chosen
+    opt = display_menu()    #displays menu and stores value in opt
+    if opt == "1":    #Option 1: display data 
       init_database()
-    elif opt == "2":
+    elif opt == "2":    #Option 2: Adds a new member to crew
       add_member(names, ranks, divs, ids)
-    elif opt == "3":
+    elif opt == "3":    #Option 3: Removes a member from crew
         remove_member(names, ranks, divs, ids)
-    elif opt == "4":
+    elif opt == "4":    #Option 4: Updates member's rank
         update_rank(names, ranks, ids)
-    elif opt == "5":
+    elif opt == "5":    #Option 5: Displays a formatted roster(table)
       display_roster(names, ranks, divs, ids)
-    elif opt == "6":
+    elif opt == "6":    #Option 6: Search for crew members
       search_crew(names, ranks, divs, ids)
-    elif opt == "7":
+    elif opt == "7":    #Option 7: Displays members from selected division
         print(f"Members in selected division: ", filter_by_division(names, divs))
-    elif opt == "8":
+    elif opt == "8":    #Option 8: Calculates and displays total crew payroll
       print("Total crew payroll: £", calculate_payroll(ranks))
-    elif opt == "9":
+    elif opt == "9":    #Option 9: Displays no of high ranking officers
       print("High ranking officers(Captains and Commanders): " , count_officers(ranks))
-    elif opt == "10":
+    elif opt == "10":   #Option 10: Exits program
       print("Shutting down.")
       break
-    else:
+    else:   #if invalid option entered, say invalid
       print("Invalid.")
 
 main()
